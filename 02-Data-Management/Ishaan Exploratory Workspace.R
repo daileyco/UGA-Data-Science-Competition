1ea008c0a6200f50ca585bbddf4acde728efdf5c
Ishaan Dave <idave@uga.edu>
2021-03-27 20:13:13 -0400

    update

--- 02-Data-Management/Ishaan Exploratory Workspace.R
+++ 02-Data-Management/Ishaan Exploratory Workspace.R
@@ -7,38 +7,52 @@ test = read.csv("Simulated_Data_Train.csv")
 valid = read.csv("Simulated_Data_Validation.csv")
 
 
-for(i in 1:ncol(train)){
+for(i in 1:(ncol(train)-1)){
   if (length(unique(train[,i])) < 6 ) {
     train[,i] = as.character(train[,i])
   }
 }
 
-for(i in 1:ncol(test)){
+
+train$out = as.factor(as.character(train$Default_ind))
+
+for(i in 1:(ncol(test)-1)){
   if (length(unique(test[,i])) < 6 ) {
     test[,i] = as.character(test[,i])
   }
 }
 
 
-for(i in 1:ncol(valid)){
+test$out = as.factor(as.character(test$Default_ind))
+
+
+for(i in 1:(ncol(valid)-1)){
   if (length(unique(valid[,i])) < 6 ) {
     valid[,i] = as.character(valid[,i])
   }
 }
+
+valid$out = as.factor(as.character(valid$Default_ind))
+
+a = target_by(train, "out")
+b = relate(a, uti_card)
+plot(b)
 library(dlookr)
 library(tidyverse)
 
-train %>%
-  eda_report(target = Default_ind, output_format = "pdf", 
+
+  eda_report(train, target = out, output_dir = "./../../03-Exploratory-Analyses/", output_format = "pdf", 
              output_file = "WF_train_EDA_ID.pdf")
 
 
-test %>%
-  eda_report(target = Default_ind, output_format = "pdf", 
+
+  eda_report(test, target = out, output_dir = "./../../03-Exploratory-Analyses/", output_format = "pdf", 
              output_file = "WF_test_EDA_ID.pdf")
 
 
-valid %>%
-  eda_report(target = Default_ind, output_format = "pdf", 
+  
+  
+
+  eda_report(valid, target = out, output_dir = "./../../03-Exploratory-Analyses/", output_format = "pdf", 
              output_file = "WF_valid_EDA_ID.pdf")
 
